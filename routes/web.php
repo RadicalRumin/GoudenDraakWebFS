370637c103356957f100205a6933fdad041fec21
RubenvanderHout <ruben.vanderhout@gmail.com>
2024-10-15 15:00:59 +0200

    Route structure

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,24 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::domain("restaurant." . env('APP_URL'))->group(function () {
+    Route::get('/', function () {
+        return dd("hello restaurant");
+    });
+});
+
+Route::domain("kassa." . env('APP_URL'))->group(function () {
+    Route::get('/', function () {
+        return dd("hello kassa");
+    });
+});
+
+Route::domain("admin." . env('APP_URL'))->group(function () {
+    Route::get('/', function () {
+        return dd("hello admin");
+    });
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
