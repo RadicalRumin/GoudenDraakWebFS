66bc771f30ca1269967f782af722c33ff17e153a
RubenvanderHout <ruben.vanderhout@gmail.com>
2024-10-15 17:53:12 +0200

    Added Tailwind

--- resources/js/Pages/Test.vue
+++ resources/js/Pages/Test.vue
@@ -9,5 +9,15 @@
 </script>
 
 <style scoped>
+    h1 {
+        color: red;
+        background-color: yellow;
+    }
+
+    input {
+        padding: 10px;
+        font-size: 16px;
+    }
+
 
 </style>
