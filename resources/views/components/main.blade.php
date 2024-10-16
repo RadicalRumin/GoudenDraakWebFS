338b1ae237b919acab6ea825cea07a13d8c99651
RubenvanderHout <ruben.vanderhout@gmail.com>
2024-10-16 15:35:01 +0200

    Added redesign start of homepage

---
+++ resources/views/components/main.blade.php
@@ -0,0 +1,91 @@
+<table id="main_table" style="padding:5px;width:100%;border-collapse: collapse">
+			<tr style="height:7px;background-color:red">
+				<td colspan="9">
+				</td>
+			<tr>
+			<tr style="height:25px;background-color:red">
+				<td width="7px">
+				</td>
+				<td style="width:25px;border-left:4px solid yellow;border-top:4px solid yellow"></td>
+				<td style="width:25px;border-right:4px solid yellow;border-top:4px solid yellow"></td>
+				<td style="width:25px;border-right:4px solid yellow;border-bottom:4px solid yellow"></td>
+				<td style="border-top:4px solid yellow;border-bottom:4px solid yellow"></td>
+				<td style="width:25px;border-left:4px solid yellow;border-bottom:4px solid yellow"></td>
+				<td style="width:25px;border-left:4px solid yellow;border-top:4px solid yellow"></td>
+				<td style="width:25px;border-right:4px solid yellow;border-top:4px solid yellow"></td>
+				<td width="7px">
+			</tr>
+			<tr style="height:25px;background-color:red">
+				<td width="7px">
+				<td style="width:25px;border-left:4px solid yellow;border-bottom:4px solid yellow"></td>
+				<td style="width:25px;border:4px solid yellow"></td>
+				<td style="width:25px;border:4px solid yellow"></td>
+				<td></td>
+				<td style="width:25px;border:4px solid yellow"></td>
+				<td style="width:25px;border:4px solid yellow"></td>
+				<td style="width:25px;border-right:4px solid yellow;border-bottom:4px solid yellow"></td>
+				<td width="7px">
+			</tr>
+			<tr style="height:25px;background-color:red">
+				<td width="7px">
+				<td style="width:25px;border-right:4px solid yellow;border-bottom:4px solid yellow"></td>
+				<td style="width:25px;border:4px solid yellow"></td>
+				<td style="width:25px"></td>
+				<td></td>
+				<td style="width:25px"></td>
+				<td style="width:25px;border:4px solid yellow"></td>
+				<td style="width:25px;border-bottom:4px solid yellow"></td>
+				<td width="7px">
+			</tr>
+			<tr style="height:50px;background-color:red">
+				<td width="7px">
+				<td style="width:25px;border-right:4px solid yellow;border-left:4px solid yellow"></td>
+				<td style="width:25px;"></td>
+				<td style="width:25px;"></td>
+				<td style="text-align:center">
+                    {{ $slot }}
+				</td>
+				<td style="width:25px;"></td>
+				<td style="width:25px;"></td>
+				<td style="width:25px;border-right:4px solid yellow;border-left:4px solid yellow"></td>
+				<td width="7px">
+			</tr>
+			<tr style="height:25px;background-color:red">
+				<td width="7px">
+				<td style="width:25px;border-right:4px solid yellow;border-top:4px solid yellow"></td>
+				<td style="width:25px;border:4px solid yellow"></td>
+				<td style="width:25px"></td>
+				<td></td>
+				<td style="width:25px"></td>
+				<td style="width:25px;border:4px solid yellow"></td>
+				<td style="width:25px;border-top:4px solid yellow"></td>
+				<td width="7px">
+			</tr>
+			<tr style="height:25px;background-color:red">
+				<td width="7px">
+				<td style="width:25px;border-left:4px solid yellow;border-top:4px solid yellow"></td>
+				<td style="width:25px;border:4px solid yellow"></td>
+				<td style="width:25px;border:4px solid yellow"></td>
+				<td></td>
+				<td style="width:25px;border:4px solid yellow"></td>
+				<td style="width:25px;border:4px solid yellow"></td>
+				<td style="width:25px;border-right:4px solid yellow;border-top:4px solid yellow"></td>
+				<td width="7px">
+			</tr>
+			<tr style="height:25px;background-color:red">
+				<td width="7px">
+				</td>
+				<td style="width:25px;border-left:4px solid yellow;border-bottom:4px solid yellow"></td>
+				<td style="width:25px;border-right:4px solid yellow;border-bottom:4px solid yellow"></td>
+				<td style="width:25px;border-right:4px solid yellow"></td>
+				<td style="border-top:4px solid yellow;border-bottom:4px solid yellow"></td>
+				<td style="width:25px;border-left:4px solid yellow;"></td>
+				<td style="width:25px;border-left:4px solid yellow;border-bottom:4px solid yellow"></td>
+				<td style="width:25px;border-right:4px solid yellow;border-bottom:4px solid yellow"></td>
+				<td width="7px">
+			</tr>
+			<tr style="height:7px;background-color:red">
+				<td colspan="9">
+				</td>
+			<tr>
+		</table>
---
+++ resources/views/components/navigation.blade.php
@@ -0,0 +1,19 @@
+<table style="margin:auto;font-size:20px;color:white" border="1px solid white">
+    <tr background={{ url('/images/menu_bg_gradient.png') }}>
+        <td valign="middle">
+            <a href="paginas/MENUKAART.html" style="color:white">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Menukaart&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </a>
+        </td>
+        <td valign="middle">
+            <a href="paginas/news.html" style="color:white">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Nieuws&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </a>
+        </td>
+        <td valign="middle">
+            <a href="paginas/contact.html" style="color:white">
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Contact&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            </a>
+        </td>
+    </tr>
+</table>
