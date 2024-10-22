16bba372b5f56e9ee80ba7826f3f1c022600e3b4
RubenvanderHout <ruben.vanderhout@gmail.com>
2024-10-22 17:12:24 +0200

    Corners bottom

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -21,6 +21,7 @@
 			</div>
 		</div>
 
+		{{-- Top Center --}}
 		<div class="grid grid-rows-[10px,30px,30px,20px]">
 			<div></div>
 			<div class="border-yellow-400 border-y-4"></div>
@@ -32,17 +33,18 @@
 		<div class="grid grid-rows-[10px,auto]">
 			<div></div>
 			<div class="grid grid-cols-[30px,30px,30px] grid-rows-[30px,30px,30px]">
+
+				<div class="border-yellow-400 border-l-4"></div>
 				<div class="border-yellow-400 border-t-4 border-l-4"></div>
 				<div class="border-yellow-400 border-t-4 border-r-4"></div>
-				<div class="border-yellow-400 border-r-4"></div>
 
-				<div class="border-yellow-400 border-b-4 border-l-4"></div>
+				<div class="border-yellow-400 border-b-4 border-t-4 border-l-4"></div>
 				<div class="border-yellow-400 border-4"></div>
-				<div class="border-yellow-400 border-y-4 border-r-4"></div>
+				<div class="border-yellow-400 border-b-4 border-r-4"></div>
 
-				<div class="border-yellow-400 border-b-4"></div>
-				<div class="border-yellow-400 border-x-4 border-b-4"></div>
 				<div></div>
+				<div class="border-yellow-400 border-x-4 border-b-4"></div>
+				<div class="border-yellow-400 border-b-4"></div>
 			</div>
 		</div>
 
@@ -62,10 +64,30 @@
 	</div>
 
 	{{-- Bottom --}}
-	<div class="bg-purple-700 h-[90px] grid grid-cols-[10px,30px,1fr,30px,10px]">
+	{{-- 10px,90px,1fr,90px,10px --}}
+	<div class="bg-purple-700 h-[90px] grid grid-cols-[10px,90px,1fr,90px,10px]">
 		<div></div>
-		<button>test</button>
 
+		{{-- Bottom Left Corner --}}
+		<div class="grid ">
+			<div class="grid grid-cols-[30px,30px,30px] grid-rows-[30px,30px,30px]">
+				<div class="border-yellow-400 border-t-4"></div>
+				<div class="border-yellow-400 border-x-4 border-t-4"></div>
+				<div></div>
+
+				<div class="border-yellow-400 border-t-4 border-l-4"></div>
+				<div class="border-yellow-400 border-t-4 border-r-4"></div>
+				<div class="border-yellow-400 border-r-4"></div>
+
+				<div class="border-yellow-400 border-b-4 border-l-4"></div>
+				<div class="border-yellow-400 border-4"></div>
+				<div class="border-yellow-400 border-y-4 border-r-4"></div>
+
+			</div>
+			<div></div>
+		</div>
+
+		{{-- Bottom Center --}}
 		<div class="grid grid-rows-[20px,30px,30px,10px]">
 			<div></div>
 			<div></div>
@@ -73,7 +95,25 @@
 			<div></div>
 		</div>
 
-		<button>test</button>
+		{{-- Bottom right Corner --}}
+		<div class="grid ">
+			<div class="grid grid-cols-[30px,30px,30px] grid-rows-[30px,30px,30px]">
+
+				<div class="border-yellow-400 border-l-4"></div>
+				<div class="border-yellow-400 border-t-4 border-l-4"></div>
+				<div class="border-yellow-400 border-t-4 border-r-4"></div>
+
+				<div class="border-yellow-400 border-b-4 border-t-4 border-l-4"></div>
+				<div class="border-yellow-400 border-4"></div>
+				<div class="border-yellow-400 border-b-4 border-r-4"></div>
+
+				<div></div>
+				<div class="border-yellow-400 border-x-4 border-b-4"></div>
+				<div class="border-yellow-400 border-b-4"></div>
+			</div>
+			<div></div>
+		</div>
+
 		<div></div>
 	</div>
 
