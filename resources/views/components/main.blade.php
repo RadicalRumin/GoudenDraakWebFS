a3f84d3452f9d0a3c8483bef908d182b17102a88
RubenvanderHout <ruben.vanderhout@gmail.com>
2024-10-22 17:34:26 +0200

    Layout done

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -74,11 +74,11 @@
 				<div class="border-yellow-400 border-t-4"></div>
 				<div class="border-yellow-400 border-t-4 border-x-4"></div>
 				<div></div>
-				
+
 				<div class="border-yellow-400 border-t-4 border-l-4"></div>
 				<div class="border-yellow-400 border-4"></div>
 				<div class="border-yellow-400 border-y-4 border-r-4"></div>
-				
+
 				<div class="border-yellow-400 border-b-4 border-l-4"></div>
 				<div class="border-yellow-400 border-r-4 border-b-4"></div>
 				<div class="border-yellow-400 border-r-4"></div>
@@ -97,18 +97,18 @@
 		{{-- Top right Corner --}}
 		<div class="grid grid-rows-[auto,10px]">
 			<div class="grid grid-cols-[30px,30px,30px] grid-rows-[30px,30px,30px]">
-				
-				<div class="border-yellow-400 border-l-4"></div>
-				<div class="border-yellow-400 border-t-4 border-l-4"></div>
-				<div class="border-yellow-400 border-t-4 border-r-4"></div>
-				
+
+				<div class="border-yellow-400"></div>
+				<div class="border-yellow-400 border-t-4 border-l-4 border-r-4"></div>
+				<div class="border-yellow-400 border-t-4"></div>
+
 				<div class="border-yellow-400 border-b-4 border-t-4 border-l-4"></div>
 				<div class="border-yellow-400 border-4"></div>
+				<div class="border-yellow-400 border-r-4 border-t-4"></div>
+
+				<div class="border-yellow-400 border-l-4"></div>
+				<div class="border-yellow-400 border-l-4 border-b-4"></div>
 				<div class="border-yellow-400 border-b-4 border-r-4"></div>
-				
-				<div></div>
-				<div class="border-yellow-400 border-x-4 border-b-4"></div>
-				<div class="border-yellow-400 border-b-4"></div>
 			</div>
 			<div></div>
 		</div>
