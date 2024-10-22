26fc41d9cd3692746ddb21c17c425ba43d5eaa16
Dylan <merelenjoy66@gmail.com>
2024-10-22 17:28:50 +0200

    de laatste corner voooooor RUBEN

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -22,7 +22,7 @@
 		</div>
 
 		{{-- Top Center --}}
-		<div class="grid grid-rows-[10px,30px,30px,20px]">
+		<div class="grid grid-rows-[10px,30px,30px,30px]">
 			<div></div>
 			<div class="border-yellow-400 border-y-4"></div>
 			<div></div>
@@ -65,48 +65,47 @@
 
 	{{-- Bottom --}}
 	{{-- 10px,90px,1fr,90px,10px --}}
-	<div class="bg-purple-700 h-[90px] grid grid-cols-[10px,90px,1fr,90px,10px]">
+	<div class="bg-purple-700 grid grid-cols-[10px,90px,1fr,90px,10px]">
 		<div></div>
 
-		{{-- Bottom Left Corner --}}
-		<div class="grid ">
+		{{-- Top Left Corner --}}
+		<div class="grid grid-rows-[auto,10px]">
 			<div class="grid grid-cols-[30px,30px,30px] grid-rows-[30px,30px,30px]">
 				<div class="border-yellow-400 border-t-4"></div>
-				<div class="border-yellow-400 border-x-4 border-t-4"></div>
+				<div class="border-yellow-400 border-t-4 border-x-4"></div>
 				<div></div>
-
+				
 				<div class="border-yellow-400 border-t-4 border-l-4"></div>
-				<div class="border-yellow-400 border-t-4 border-r-4"></div>
-				<div class="border-yellow-400 border-r-4"></div>
-
-				<div class="border-yellow-400 border-b-4 border-l-4"></div>
 				<div class="border-yellow-400 border-4"></div>
 				<div class="border-yellow-400 border-y-4 border-r-4"></div>
-
+				
+				<div class="border-yellow-400 border-b-4 border-l-4"></div>
+				<div class="border-yellow-400 border-r-4 border-b-4"></div>
+				<div class="border-yellow-400 border-r-4"></div>
 			</div>
 			<div></div>
 		</div>
 
-		{{-- Bottom Center --}}
-		<div class="grid grid-rows-[20px,30px,30px,10px]">
+		{{-- Top Center --}}
+		<div class="grid grid-rows-[30px,30px,30px,10px]">
 			<div></div>
 			<div></div>
 			<div class="border-yellow-400 border-y-4"></div>
 			<div></div>
 		</div>
 
-		{{-- Bottom right Corner --}}
-		<div class="grid ">
+		{{-- Top right Corner --}}
+		<div class="grid grid-rows-[auto,10px]">
 			<div class="grid grid-cols-[30px,30px,30px] grid-rows-[30px,30px,30px]">
-
+				
 				<div class="border-yellow-400 border-l-4"></div>
 				<div class="border-yellow-400 border-t-4 border-l-4"></div>
 				<div class="border-yellow-400 border-t-4 border-r-4"></div>
-
+				
 				<div class="border-yellow-400 border-b-4 border-t-4 border-l-4"></div>
 				<div class="border-yellow-400 border-4"></div>
 				<div class="border-yellow-400 border-b-4 border-r-4"></div>
-
+				
 				<div></div>
 				<div class="border-yellow-400 border-x-4 border-b-4"></div>
 				<div class="border-yellow-400 border-b-4"></div>
@@ -116,5 +115,6 @@
 
 		<div></div>
 	</div>
+
 
 </main>
