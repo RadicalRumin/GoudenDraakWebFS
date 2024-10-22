8e9187859222e3b61d94d6e7df8cad80542bb3fa
Dylan <merelenjoy66@gmail.com>
2024-10-22 16:46:55 +0200

    top right corner prepared for ruben :)

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -2,6 +2,8 @@
 	{{-- Top --}}
 	<div class="bg-pink-500 grid grid-cols-[10px,90px,1fr,90px,10px]">
 		<div></div>
+
+		{{-- Top Left Corner --}}
 		<div class="grid grid-rows-[10px,auto]">
 			<div></div>
 			<div class="grid grid-cols-[30px,30px,30px] grid-rows-[30px,30px,30px]">
@@ -26,7 +28,24 @@
 			<div></div>
 		</div>
 
-		<button>test</button>
+		{{-- Top right Corner --}}
+		<div class="grid grid-rows-[10px,auto]">
+			<div></div>
+			<div class="grid grid-cols-[30px,30px,30px] grid-rows-[30px,30px,30px]">
+				<div class="border-yellow-400 border-t-4 border-l-4"></div>
+				<div class="border-yellow-400 border-t-4 border-r-4"></div>
+				<div class="border-yellow-400 border-r-4"></div>
+
+				<div class="border-yellow-400 border-b-4 border-l-4"></div>
+				<div class="border-yellow-400 border-4"></div>
+				<div class="border-yellow-400 border-y-4 border-r-4"></div>
+
+				<div class="border-yellow-400 border-b-4"></div>
+				<div class="border-yellow-400 border-x-4 border-b-4"></div>
+				<div></div>
+			</div>
+		</div>
+
 		<div></div>
 	</div>
 
