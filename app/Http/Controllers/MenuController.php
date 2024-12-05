4e6dac841fbae47de8e56a9fce6272af6ea60df2
RubenvanderHout <ruben.vanderhout@gmail.com>
2024-12-05 14:24:43 +0100

    Refactor

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Dish;
-use Barryvdh\DomPDF\Facade\Pdf as Pdf;
+use Barryvdh\DomPDF\Facade\Pdf;
 
 class MenuController extends Controller
 {
